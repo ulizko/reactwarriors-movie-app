ae1351d120fabf1db61209e19f5cc1d0101b8fe8
Oleksandr Ulizko <au@wizardry.ua>
2019-12-09 18:17:42 +0200

    add proptypes

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import SortBy from "./SortBy";
 export default class Filters extends React.Component {
   render() {
     const {
@@ -8,23 +8,10 @@ export default class Filters extends React.Component {
       onChangePage,
       page,
     } = this.props;
+
     return (
       <form className="mb-3">
-        <div className="form-group">
-          <label htmlFor="sort_by">Сортировать по:</label>
-          <select
-            className="form-control"
-            id="sort_by"
-            name="sort_by"
-            value={sort_by}
-            onChange={onChangeFilters}
-          >
-            <option value="popularity.desc">Популярные по убыванию</option>
-            <option value="popularity.asc">Популярные по возростанию</option>
-            <option value="vote_average.desc">Рейтинг по убыванию</option>
-            <option value="vote_average.asc">Рейтинг по возростанию</option>
-          </select>
-        </div>
+        <SortBy sort_by={sort_by} onChangeFilters={onChangeFilters} />
         <div className="btn-group">
           <button
             type="button"
