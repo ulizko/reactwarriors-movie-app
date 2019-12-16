26a4ebd9d20fe46e96c53917fe8b3f705c9e0b60
Oleksandr Ulizko <au@wizardry.ua>
2019-12-16 17:37:07 +0200

    use promises for chaining requests to api

--- src/components/Header/Login/Login.jsx
+++ src/components/Header/Login/Login.jsx
@@ -2,12 +2,28 @@ import React, { Component } from "react";
 import { API_URL, API_KEY_3 } from "../../../api/api";
 export default class Login extends Component {
   sendPromises = () => {
-    const link = `${API_URL}/authentication/token/new?api_key=${API_KEY_3}`;
-    fetch(link)
-      .then(response => response.json())
+    const fetchApi = (url, options = {}) => {
+      return new Promise((resolve, reject) => {
+        fetch(url, options)
+          .then(response => {
+            if (response.status < 400) {
+              return response.json();
+            } else {
+              throw response;
+            }
+          })
+          .then(data => {
+            resolve(data);
+          })
+          .catch(data => {
+            reject(data);
+          });
+      });
+    };
+
+    fetchApi(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
       .then(data => {
-        console.log(data);
-        fetch(
+        return fetchApi(
           `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
           {
             method: "POST",
@@ -21,28 +37,26 @@ export default class Login extends Component {
               request_token: data.request_token,
             }),
           }
-        )
-          .then(response => response.json())
-          .then(data => {
-            console.log(data);
-            fetch(
-              `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
-              {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                  "Content-type": "application/json",
-                },
-                body: JSON.stringify({
-                  request_token: data.request_token,
-                }),
-              }
-            )
-              .then(response => response.json())
-              .then(data => {
-                console.log(data);
-              });
-          });
+        );
+      })
+      .then(data => {
+        return fetchApi(
+          `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
+          {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              request_token: data.request_token,
+            }),
+          }
+        );
+      })
+      .then(data => console.log(data))
+      .catch(error => {
+        console.log("error", error);
       });
   };
   render() {
