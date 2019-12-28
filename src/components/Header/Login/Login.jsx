99fa831e3f3b34c0f1b9bbce998a7dfc7d1f00c0
Oleksandr Ulizko <ulizman@gmail.com>
2019-12-28 23:18:53 +0200

    add remote authentication

--- src/components/Header/Login/Login.jsx
+++ src/components/Header/Login/Login.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { Modal, ModalBody } from "reactstrap";
 import LoginForm from "./LoginForm";
-import { API_URL, API_KEY_3 } from "../../../api/api";
-import { timingSafeEqual } from "crypto";
 export default class Login extends Component {
   constructor() {
     super();
@@ -16,64 +14,6 @@ export default class Login extends Component {
     this.setState(prevState => ({ showModal: !prevState.showModal }));
   };
 
-  sendPromises = () => {
-    const fetchApi = (url, options = {}) => {
-      return new Promise((resolve, reject) => {
-        fetch(url, options)
-          .then(response => {
-            if (response.status < 400) {
-              return response.json();
-            } else {
-              throw response;
-            }
-          })
-          .then(data => {
-            resolve(data);
-          })
-          .catch(data => {
-            reject(data);
-          });
-      });
-    };
-
-    fetchApi(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
-      .then(data => {
-        return fetchApi(
-          `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
-          {
-            method: "POST",
-            mode: "cors",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify({
-              username: process.env.REACT_APP_TMDB_LOGIN,
-              password: process.env.REACT_APP_TMDB_PASSWORD,
-              request_token: data.request_token,
-            }),
-          }
-        );
-      })
-      .then(data => {
-        return fetchApi(
-          `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
-          {
-            method: "POST",
-            mode: "cors",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify({
-              request_token: data.request_token,
-            }),
-          }
-        );
-      })
-      .then(data => console.log(data))
-      .catch(error => {
-        console.log("error", error);
-      });
-  };
   render() {
     return (
       <React.Fragment>
--- src/components/Header/Login/LoginForm.jsx
+++ src/components/Header/Login/LoginForm.jsx
@@ -1,31 +1,154 @@
 import React, { Component } from "react";
+import { API_URL, API_KEY_3 } from "../../../api/api";
 
 export default class LoginForm extends Component {
   constructor() {
     super();
     this.state = {
+      submitting: false,
       username: "",
       password: "",
       errors: {},
     };
   }
 
-  onChange = event => {
-    this.setState();
+  onSubmit = () => {
+    const fetchApi = (url, options = {}) => {
+      return new Promise((resolve, reject) => {
+        fetch(url, options)
+          .then(response => {
+            if (response.status < 400) {
+              return response.json();
+            } else {
+              throw response;
+            }
+          })
+          .then(data => {
+            resolve(data);
+          })
+          .catch(data => {
+            data.json().then(error => {
+              reject(error);
+
+            })
+          });
+      });
+    };
+
+    this.setState({ submitting: true })
+
+    fetchApi(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
+      .then(data => {
+        return fetchApi(
+          `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
+          {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              username: this.state.username,
+              password: this.state.password,
+              request_token: data.request_token,
+            }),
+          }
+        );
+      })
+      .then(data => {
+        return fetchApi(
+          `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
+          {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              request_token: data.request_token,
+            }),
+          }
+        );
+      })
+      .then(data => {
+        this.setState({submitting: false})
+        console.log(data)
+      })
+      .catch(error => {
+        this.setState({
+          submitting: false,
+          errors: {
+            base: error.status_message
+          }
+        })
+        console.log("error", error);
+      });
   };
+
+  onChange = event => {
+    const { name, value } = event.target
+    this.setState(prevState => {
+      return {
+        [name]: value,
+        errors: { ...prevState.errors, [name]: null, base: null }
+      }
+    });
+  };
+
+  handleBlur = (event) => {
+    const errors = this.validateFields()
+    if(Object.keys(errors).length) {
+      this.setState(prevState => {
+        return {
+          errors: {
+            ...prevState.errors,
+            ...errors
+          }
+        }
+      })
+    }
+  }
+
+  validateFields = () => {
+    const errors = {}
+
+    if (!this.state.username) {
+      errors.username = 'Not empty'
+    }
+
+    return errors
+  }
+
+  onLogin = (e) => {
+    e.preventDefault()
+    const errors = this.validateFields()
+    if(Object.keys(errors).length) {
+      this.setState({errors})
+
+    } else {
+      this.onSubmit()
+    }
+  }
+
   render() {
+    const { username, password, errors, submitting } = this.state
     return (
       <form>
         <h1 className="h3 mb-3 font-weight-normal text-center">Авторизация</h1>
         <div className="form-group">
           <label htmlFor="email">Пользователь</label>
           <input
-            type="email"
+            type="text"
             className="form-control"
-            id="email"
+            id="username"
+            name="username"
             aria-describedby="emailHelp"
             placeholder="Пользователь"
+            value={username}
+            onChange={this.onChange}
+            onBlur={this.handleBlur}
           />
+          {errors.username && <div className="invalid-feedback">{errors.username}</div>}
         </div>
         <div className="form-group">
           <label htmlFor="password">Пароль</label>
@@ -33,12 +156,16 @@ export default class LoginForm extends Component {
             type="password"
             className="form-control"
             id="password"
+            name="password"
             placeholder="Пароль"
+            value={password}
+            onChange={this.onChange}
           />
         </div>
-        <button type="submit" className="btn btn-primary btn-block">
+        <button type="submit" className="btn btn-primary btn-block" onClick={this.onLogin} disabled={submitting}>
           Вход
         </button>
+        {errors.base && <div className="invalid-feedback text-center">{errors.base}</div>}
       </form>
     );
   }
